bced7038b85f87ff156b1e5372212fd3f09a0bda
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-10-12 15:45:21 -0600

    Created express app

--- server.js
+++ server.js
@@ -3,17 +3,17 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
-const port = 3000;
+
 
 // Middleware
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
 // Controller
-const bballTeamController = require("");
+// const bballTeamController = require("");
 
 
-
+const port = 3000;
 app.listen(port, () => {
     console.log(`listening to port ${port}`)
 })
