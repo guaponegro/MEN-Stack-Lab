0769553951816f0b4c8e713942af38b35381ec5f
Steven Ariol <stevenariol@Stevens-MacBook-Air.local>
2018-10-12 15:51:43 -0600

    linked mongo

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
-
+require("./db/db");
 
 // Middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
 // Controller
-// const bballTeamController = require("");
+const bballTeamController = require("./controllers/bball");
 
 
 const port = 3000;
